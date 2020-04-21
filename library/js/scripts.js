cf02c8519d6c9a9affa2b8c7278833d1abef200f
Jess Izen <jackliebiz@gmail.com>
2020-04-21 16:23:33 -0400

    Update scripts.js

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -183,32 +183,5 @@ jQuery(document).ready(function($) {
 });
 
 
-//helper function to find next element of matching selector (ie next listen button)
-function nextInDOM(_selector, _subject) {
-  var next = getNext(_subject);
-  while(next.length != 0) {
-      var found = searchFor(_selector, next);
-      if(found != null) return found;
-      next = getNext(next);
-  }
-  return null;
-}
-function getNext(_subject) {
-  if(_subject.next().length > 0) return _subject.next();
-  return getNext(_subject.parent());
-}
-function searchFor(_selector, _subject) {
-  if(_subject.is(_selector)) return _subject;
-  else {
-      var found = null;
-      _subject.children().each(function() {
-          found = searchFor(_selector, $(this));
-          if(found != null) return false;
-      });
-      return found;
-  }
-  return null; // will/should never get here
-}
-
 
 /* end of as page load scripts */
